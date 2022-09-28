cd332cfa82768da027075e2340e7388f5a40ec4b
gaeun23 <0423g@naver.com>
2022-09-28 20:06:01 +0900

    Bottom Navi widgets, Tab Container 뷰의 활용

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue,
         fontFamily: 'Cabin',
-        primarySwatch: Colors.brown,
+        // Bottom Navi 의 Selected Color 는 primarySwatch 값으로 지정
+        primarySwatch: Colors.blue,
       ),
       // Home 속성은 pages 를 할당
       home: MyHomePage(),
@@ -50,10 +51,21 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
         body: TabBarView(
-          // PageView Widget 은 ViewPager 와 동일
+          // TabBarView 의 children 으로는 Tab widget 으로 감싸주는 것이 좋음
           children: [
-            Container(
-              color: Colors.red,
+            Tab(
+              // Center 위젯으로 감싸주면 사용하면 뷰의 정가운데에 위치하게 할 수 있음
+              // Padding 위젯으로 감싸주면 Padding 값을 지정할 수 있음
+              // Padding 위젯과 높이를 지정하지 않는 Container 를 사용하면 inner margin 값이 지정된 것처럼 보이게 됨
+              // Align 위젯으로 감싸주면 alignment 속성을 통해서 특정 위치로 정렬할 수 있음
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+              ),
             ),
             Container(
               color: Colors.blue,
@@ -64,6 +76,13 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
         drawer: const Drawer(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
+          ],
+        ),
       ),
     );
   }
