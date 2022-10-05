68d8936addd6e23d21b6afa3890550f631a524f6
gaeun23 <0423g@naver.com>
2022-10-06 00:50:54 +0900

    checkBox widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Home Page'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
+      ),
+      body: Checkbox(
+        value: _isChecked,
+        onChanged: (value) {
+          setState(() {
+            _isChecked = value!;
+          });
+        },
+      ),
+      drawer: const Drawer(),
     );
   }
 }
