f86302a83bb12886f8d163a16b529c2a786541d1
gaeun23 <0423g@naver.com>
2022-10-12 20:05:24 +0900

    7주차 강의 2-Global Key, Form

--- lib/main.dart
+++ lib/main.dart
@@ -29,49 +29,180 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List list = List.generate(20, (index) => index);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Using Key'),
-          actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
-        ),
+      appBar: AppBar(
+        title: const Text('Using Key'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+      body: MyForm(),
+    );
+  }
+}
 
-        // ListView 의 필수 속성 : itemBuilder 와 itemCount
-        // ReorderableListView 의 필수 속성 : onReorder
-        // ReorderableListView 의 모든 item은 반드시 key를 가져야 함.
+class MyForm extends StatefulWidget {
+  const MyForm({Key? key}) : super(key: key);
 
-        // Dismissible widget은 반드시 key를 가져야 함
-        body: ReorderableListView.builder(
-          itemBuilder: (context, i) {
-            return Dismissible(
-              key: ValueKey(list[i]),
-              background: Container(
-                color: Colors.orange,
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  StudentResult studentResult = StudentResult(0, 0, 0, 0, false);
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Mid-Term Exam',
               ),
-              child: ListTile(
-                title: Text('Students ${list[i]}'),
-                leading: const Icon(Icons.person),
-                trailing: const Icon(Icons.navigate_next),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Insert some Texts';
+                } else if (int.tryParse(value) == null) {
+                  return 'Insert some Integer';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                studentResult.midTermExam = int.parse(value!);
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Final Exam',
               ),
-              onDismissed: (direction) {
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Insert some Texts';
+                } else if (int.tryParse(value) == null) {
+                  return 'Insert some Integer';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                studentResult.finalTermExam = int.parse(value!);
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Additional Point',
+              ),
+              value: studentResult.additionalPoint,
+              items: List.generate(11, (index) {
+                if (index == 0) {
+                  return DropdownMenuItem(
+                    value: index,
+                    child: const Text('Choose the additional point'),
+                  );
+                }
+                return DropdownMenuItem(
+                  value: index,
+                  child: Text('${index - 1} point'),
+                );
+              }),
+              onChanged: (value) {
                 setState(() {
-                  list.removeAt(i);
+                  studentResult.additionalPoint = value! - 1;
                 });
               },
-            );
-          },
-          itemCount: list.length,
-          onReorder: (int oldIndex, int newIndex) {
-            setState(() {
-              if (oldIndex < newIndex) {
-                newIndex -= 1;
-              }
-              list.insert(newIndex, list.removeAt(oldIndex));
-            });
-          },
-        ));
+              validator: (value) {
+                if (value == 0) {
+                  return 'Please select the point';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RadioListTile(
+                title: const Text('Team leader (+10)'),
+                value: 10,
+                groupValue: studentResult.teamLeaderPoint,
+                onChanged: (value) {
+                  setState(() {
+                    studentResult.teamLeaderPoint = 10;
+                  });
+                }),
+            RadioListTile(
+                title: const Text('Team Member'),
+                value: 0,
+                groupValue: studentResult.teamLeaderPoint,
+                onChanged: (value) {
+                  setState(() {
+                    studentResult.teamLeaderPoint = 0;
+                  });
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            CheckboxListTile(
+                title: const Text('Absence less than 4'),
+                value: studentResult.attendance,
+                onChanged: (value) {
+                  setState(() {
+                    studentResult.attendance = value!;
+                  });
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Processing data'),
+                    ));
+                    _formKey.currentState!.save();
+                    print(studentResult);
+                  }
+                });
+              },
+              child: const Text('Enter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StudentResult {
+  int midTermExam;
+  int finalTermExam;
+  int teamLeaderPoint;
+  int additionalPoint;
+  bool attendance;
+
+  StudentResult(this.midTermExam, this.finalTermExam, this.teamLeaderPoint,
+      this.additionalPoint, this.attendance);
+
+  @override
+  String toString() {
+    return '('
+        '$midTermExam, '
+        '$finalTermExam, '
+        '$teamLeaderPoint, '
+        '$additionalPoint, '
+        '$attendance)';
   }
 }
