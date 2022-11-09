b09fcd1afc73a46cc2a53415576fc5cd9c7bc3fa
gaeun23 <0423g@naver.com>
2022-11-09 16:34:01 +0900

    week10-counter app init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,39 @@
 import 'package:flutter/material.dart';
 
-enum Role { leader, member }
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.brown,
+        primarySwatch: Colors.blue,
       ),
-      // 슬래쉬는 가장 기본이 되는 home view 를 의미
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const MyHomePage(),
-        '/measure': (context) => const MeasurePage(),
-        '/milk': (context) => const MilkPage(),
-        '/sweetCoffee': (context) => const SweetCoffeePage(),
-        '/nonCoffee': (context) => const NonCoffeePage(),
-        '/coffeeAgain': (context) => const CoffeeAgainPage(),
-        '/result': (context) => const ResultPage(),
-      },
-      // home: const MyHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const StartPage();
-  }
-}
+  int _counter = 0;
 
-class StartPage extends StatefulWidget {
-  const StartPage({Key? key}) : super(key: key);
-
-  @override
-  State<StartPage> createState() => _StartPageState();
-}
-
-class _StartPageState extends State<StartPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Choose Beverage')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'I want help you choose the beverage.',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Let\'s start!',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    Navigator.pushNamed(context, '/measure');
-                  });
-                },
-                child: const Text('Go'))
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MeasurePage extends StatefulWidget {
-  const MeasurePage({Key? key}) : super(key: key);
-
-  @override
-  State<MeasurePage> createState() => _MeasurePageState();
-}
-
-class _MeasurePageState extends State<MeasurePage> {
-  int cup = 0;
-
-  void minusCup() {
+  void _incrementCounter() {
     setState(() {
-      cup--;
-    });
-  }
-
-  void plusCup() {
-    setState(() {
-      cup++;
+      _counter++;
     });
   }
 
@@ -116,335 +41,27 @@ class _MeasurePageState extends State<MeasurePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Measure Your Coffee'),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: <Widget>[
             const Text(
-              'How many cups of coffee did you drink?',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
+              'You have pushed the button this many times:',
             ),
             Text(
-              '$cup cups',
-              style: const TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: minusCup,
-                  child: const Text(
-                    '-',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: plusCup,
-                  child: const Text(
-                    '+',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (cup > 1) {
-                      Navigator.pushNamed(context, '/nonCoffee');
-                    } else {
-                      Navigator.pushNamed(context, '/milk');
-                    }
-                  });
-                },
-                child: const Text('Next'))
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MilkPage extends StatelessWidget {
-  const MilkPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Milk'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Since you drank 1 cup of coffee,',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'you may want coffee.',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Do you want milk in the coffee?',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/sweetCoffee');
-                  },
-                  child: const Text(
-                    'Yes',
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/result',
-                        arguments: 'Americano');
-                  },
-                  child: const Text(
-                    'No',
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SweetCoffeePage extends StatelessWidget {
-  const SweetCoffeePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SweetCoffee'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Do you want sweet coffee?',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/result',
-                        arguments: 'Mocha Latte');
-                  },
-                  child: const Text(
-                    'Yes',
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/result',
-                        arguments: 'Caffe Latte');
-                  },
-                  child: const Text(
-                    'No',
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NonCoffeePage extends StatelessWidget {
-  const NonCoffeePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Juice or Latte'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Since you drank 2 cup of coffee,',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'you may not want coffee.',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Do you want juice or latte?',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/result',
-                        arguments: 'Grapefruit Juice');
-                  },
-                  child: const Text(
-                    'Juice',
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/coffeeAgain');
-                  },
-                  child: const Text(
-                    'Latte',
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CoffeeAgainPage extends StatelessWidget {
-  const CoffeeAgainPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Coffee Again'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Do you want some more coffee?',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/sweetCoffee');
-                  },
-                  child: const Text(
-                    'Yes',
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/result',
-                        arguments: 'Sweet Potato Latte');
-                  },
-                  child: const Text(
-                    'No',
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ResultPage extends StatelessWidget {
-  const ResultPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final result = ModalRoute.of(context)?.settings.arguments as String;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Result'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Your best beverage is',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              result,
-              style: const TextStyle(fontSize: 24),
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).popUntil((route) => route.isFirst);
-        },
-        child: const Icon(Icons.refresh),
-      ),
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
