6d14de1699a5d369a392190e6a0cfdb3166ebaea
gaeun23 <0423g@naver.com>
2022-11-10 12:43:59 +0900

    week10-firebase login 된 정보 가져오기

--- lib/ChatPage.dart
+++ lib/ChatPage.dart
@@ -1,14 +1,41 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class ChatPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
 
   static const routeName = '/chat';
 
   @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggedUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _authentication.currentUser;
+      if (user != null) {
+        loggedUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final userEmail = ModalRoute.of(context)!.settings.arguments as String;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
@@ -25,7 +52,7 @@ class ChatPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              userEmail,
+              loggedUser!.email!,
               style: const TextStyle(fontSize: 20),
             ),
           ],
