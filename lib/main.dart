b27f6419e8f8123afe8ee78a478e66bdc15e5ad6
gaeun23 <0423g@naver.com>
2022-11-23 18:06:55 +0900

    week12-provider import 추가, 전역 State class 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_widgets/scorepage.dart';
+import 'package:provider/provider.dart';
+import 'scores.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -10,12 +12,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
+    // Score 라는 전역State 를 사용할 수 있도록 선언
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Scores(),
+        )
+      ],
+      // builder 말고 child 라고 사용해도 되지만, 더 안전하게 사용하기 위해 builder 로 지정
+      builder: (context, child) => MaterialApp(
+        title: 'My App',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: ScorePage(),
       ),
-      home: ScorePage(),
     );
   }
 }
---
+++ lib/scores.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Scores with ChangeNotifier {
+  int midTermExam = 30;
+  int finalExam = 30;
+}
